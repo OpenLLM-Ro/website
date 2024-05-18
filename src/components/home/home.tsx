aee5b5e4f008665f70b664c742ea5178952efb29
Loila11 <miruna_z@yahoo.com>
2024-05-18 19:31:09 +0300

    Add subtitle

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -4,14 +4,6 @@ import { FaGithub } from 'react-icons/fa';
 import { NewsBulletin } from './news';
 import hfIcon from './hf-logo.svg';
 
-const Title = () => {
-  return (
-    <Heading paddingTop={['25vh']} as={'h1'} size={['2xl', '4xl']}>
-      OpenLLM-Ro
-    </Heading>
-  );
-};
-
 const Buttons = () => (
   <HStack
     padding={['3vh 10vw', '5vh 10vw 0']}
@@ -35,7 +27,12 @@ const Buttons = () => (
 export const Home = () => {
   return (
     <Stack id={'home'} minHeight={'90vh'} justifyContent={'center'} alignItems={'center'}>
-      <Title />
+      <Heading paddingTop={['25vh']} as={'h1'} size={['2xl', '4xl']}>
+        OpenLLM-Ro
+      </Heading>
+      <Heading padding={'0 5vw'} as={'h2'} size={['l', 'xl']} textAlign={'center'}>
+        Join the community building open LLMs for Romanian:
+      </Heading>
       <Buttons />
       <NewsBulletin />
     </Stack>
