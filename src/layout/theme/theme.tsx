c5e0a67d012b7a127485881429cdc74b8ac5d842
Loila11 <miruna_z@yahoo.com>
2024-05-18 11:04:19 +0300

    Set up dark mode

--- src/layout/theme/theme.tsx
+++ src/layout/theme/theme.tsx
@@ -1,8 +1,14 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 import '@fontsource/montserrat';
 import '@fontsource/montserrat/600.css';
 
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
 export const theme = extendTheme({
+  ...config,
   fonts: {
     body: `'Montserrat', sans-serif`,
     heading: `'Montserrat', sans-serif`,
