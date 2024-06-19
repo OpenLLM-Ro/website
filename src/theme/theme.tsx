d7df12c07b2d0cee0dc7011a22a7d96abb21f27e
Miruna-Andreea Zavelca <miruna_z@yahoo.com>
2024-06-19 11:33:36 +0300

    Fix background

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,6 +1,14 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        color: 'whiteAlpha.900',
+        bg: 'gray.800',
+      },
+    },
+  },
   components: {
     Heading: {
       baseStyle: {
