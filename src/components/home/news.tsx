e97edaef4082a3960c70c710de8bda3baa93fb20
Mihai Masala <mihaimasala@gmail.com>
2024-06-28 01:08:38 +0300

    Fix date

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -13,13 +13,13 @@ export const NewsBulletin = () => (
       News:
     </Text>
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rogemma-667d261f6b607a23173c1e73'}>
-      27.06.2024 - launched a new Romanian model fine-tuned on Gemma
+      28.06.2024 - launched a new Romanian model fine-tuned on Gemma
     </News>
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rollama3-667d25e2db6a8e980f0b1551'}>
-      27.06.2024 - launched a new Romanian model fine-tuned on Llama3
+      28.06.2024 - launched a new Romanian model fine-tuned on Llama3
     </News>
     <News link={'https://arxiv.org/abs/2406.18266'}>
-      27.06.2024 - new preprint adding more models and evaluations
+      28.06.2024 - new preprint adding more models and evaluations
     </News>
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/romistral-6647241bf2cbbe9fd8c2afa4'}>
       17.05.2024 - launched a new Romanian model fine-tuned on Mistral
