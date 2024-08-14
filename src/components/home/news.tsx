163fd041ac91eb7b386629530fb11373772e5662
MihaiMasala <mihaimasala@gmail.com>
2024-08-14 16:58:56 +0300

    Update news.tsx

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -12,26 +12,38 @@ export const NewsBulletin = () => (
     <Text as={'kbd'} fontWeight={'bold'}>
       News:
     </Text>
+    
+    <News link={'https://github.com/OpenLLM-Ro/LLaMA-Factory'}>
+      28.06.2024 - released the code & data for training Romanian LLMs
+    </News>
+
     <News link={'https://github.com/OpenLLM-Ro/lm-evaluation-harness'}>
       28.06.2024 - released the code & data for evaluating Romanian LLMs on academic benchmarks and downstream tasks
     </News>
+    
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rollama3-667d25e2db6a8e980f0b1551'}>
       28.06.2024 - launched a new Romanian model fine-tuned on Llama3
     </News>
+    
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rogemma-667d261f6b607a23173c1e73'}>
       28.06.2024 - launched a new Romanian model fine-tuned on Gemma
     </News>
+    
     <News link={'https://arxiv.org/abs/2406.18266'}>
       27.06.2024 - new preprint adding more models and evaluations
     </News>
+    
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/romistral-6647241bf2cbbe9fd8c2afa4'}>
       17.05.2024 - launched a new Romanian model fine-tuned on Mistral
     </News>
+    
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rollama2-664722bbf536ec14701ec81d'}>
       14.05.2024 - launched the first Romanian models fine-tuned on Llama2
     </News>
+    
     <News link={'/comunicat.pdf'}>
       14.05.2024 - press release (in Romanian) regarding the launch of OpenLLM-Ro
     </News>
+    
   </Stack>
 );
