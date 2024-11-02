6e7b72fd9bfeb55e7e5c6cc5868955819db56494
Alex Renoki <alex@renoki.org>
2024-11-02 12:59:22 +0200

    Added dynamic year

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,6 +3,6 @@ import { Text } from '@chakra-ui/react';
 
 export const Footer: React.FC = () => (
   <Text minHeight={'10vh'} padding={'0 5vw'} align={'center'} alignContent={'center'}>
-    Copyright @ OpenLLM-Ro 2024
+    Copyright @ OpenLLM-Ro {new Date().getFullYear()}
   </Text>
 );
