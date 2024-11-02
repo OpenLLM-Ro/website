80d819c1931cfa988a733f3fb64805abd213095f
Alex Renoki <alex@renoki.org>
2024-11-02 11:18:56 +0200

    Format with `npm run format`

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -16,13 +16,14 @@ export const NewsBulletin = () => (
     <News link={'https://huggingface.co/OpenLLM-Ro'}>
       09.10.2024 - released new instruct and human aligned models
     </News>
-    
+
     <News link={'https://ilds.ro/workshop-llms-for-romanian'}>
       28.09.2024 - workshop LLMs for Romanian
     </News>
-    
+
     <News link={'https://github.com/OpenLLM-Ro/FastChat'}>
-      26.08.2024 - released the code & data (RoMTBench and RoCulturaBench) for evaluating Romanian LLMs using LLM-as-a-Judge
+      26.08.2024 - released the code & data (RoMTBench and RoCulturaBench) for evaluating Romanian
+      LLMs using LLM-as-a-Judge
     </News>
 
     <News link={'https://github.com/OpenLLM-Ro/LLaMA-Factory'}>
@@ -30,32 +31,32 @@ export const NewsBulletin = () => (
     </News>
 
     <News link={'https://github.com/OpenLLM-Ro/lm-evaluation-harness'}>
-      09.08.2024 - released the code & data for evaluating Romanian LLMs on academic benchmarks and downstream tasks
+      09.08.2024 - released the code & data for evaluating Romanian LLMs on academic benchmarks and
+      downstream tasks
     </News>
-    
+
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rollama3-667d25e2db6a8e980f0b1551'}>
       28.06.2024 - launched a new Romanian model fine-tuned on Llama3
     </News>
-    
+
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rogemma-667d261f6b607a23173c1e73'}>
       28.06.2024 - launched a new Romanian model fine-tuned on Gemma
     </News>
-    
+
     <News link={'https://arxiv.org/abs/2406.18266'}>
       27.06.2024 - new preprint adding more models and evaluations
     </News>
-    
+
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/romistral-6647241bf2cbbe9fd8c2afa4'}>
       17.05.2024 - launched a new Romanian model fine-tuned on Mistral
     </News>
-    
+
     <News link={'https://huggingface.co/collections/OpenLLM-Ro/rollama2-664722bbf536ec14701ec81d'}>
       14.05.2024 - launched the first Romanian models fine-tuned on Llama2
     </News>
-    
+
     <News link={'/comunicat.pdf'}>
       14.05.2024 - press release (in Romanian) regarding the launch of OpenLLM-Ro
     </News>
-    
   </Stack>
 );
