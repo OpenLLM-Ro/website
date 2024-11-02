d5f31036209ba2dad736cb6776f2eec753237e9a
Alex Renoki <alex@renoki.org>
2024-11-02 13:03:01 +0200

    Refactoring: rewording, more space, group news into month-year

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -1,62 +1,127 @@
 import React, { ReactNode } from 'react';
 import { Stack, Text } from '@chakra-ui/react';
 
-const News: React.FC<{ link: string; children: ReactNode }> = ({ link, children }) => (
+const NewsEntry: React.FC<{ link: string; children: ReactNode }> = ({ link, children }) => (
   <Text variant={'news'} as={'a'} target={'_blank'} rel={'noreferrer'} href={link}>
     {children}
   </Text>
 );
 
-export const NewsBulletin = () => (
-  <Stack padding={['8vh 10vw']}>
-    <Text as={'kbd'} fontWeight={'bold'}>
-      News:
-    </Text>
+export const NewsBulletin = () => {
+  const entries: { link: string; text: string; type: string; date: string }[] = [
+    {
+      link: 'https://huggingface.co/OpenLLM-Ro',
+      date: '2024-10-09',
+      type: 'Release',
+      text: 'New instruct and human-aligned models',
+    },
+    {
+      link: 'https://ilds.ro/workshop-llms-for-romanian',
+      date: '2024-09-28',
+      type: 'Event',
+      text: 'Workshop about Romanian LLMs',
+    },
+    {
+      link: 'https://github.com/OpenLLM-Ro/FastChat',
+      date: '2024-08-26',
+      type: 'Code',
+      text: 'Open sourced the code and data used for evaluating Romanian LLMs using LLM-as-a-Judge',
+    },
+    {
+      link: 'https://github.com/OpenLLM-Ro/LLaMA-Factory',
+      date: '2024-08-14',
+      type: 'Code',
+      text: 'Open sourced the code and data used to train Romanian LLMs.',
+    },
+    {
+      link: 'https://github.com/OpenLLM-Ro/lm-evaluation-harness',
+      date: '2024-08-09',
+      type: 'Code',
+      text: 'Open sourced the code and data for evaluating Romanian LLMs on academic benchmarks and downstream tasks.',
+    },
+    {
+      link: 'https://huggingface.co/collections/OpenLLM-Ro/rollama3-667d25e2db6a8e980f0b1551',
+      date: '2024-06-28',
+      type: 'Fine-Tune Release',
+      text: 'New fine-tuned model based on Llama-3: OpenLLM-Ro/rollama3',
+    },
+    {
+      link: 'https://huggingface.co/collections/OpenLLM-Ro/rogemma-667d261f6b607a23173c1e73',
+      date: '2024-06-28',
+      type: 'Fine-Tune Release',
+      text: 'New fine-tuned model based on Gemma: OpenLLM-Ro/rogemma',
+    },
+    {
+      link: 'https://arxiv.org/abs/2406.18266',
+      date: '2024-06-27',
+      type: 'Paper',
+      text: 'New pre-print containing more models and evaluations.',
+    },
+    {
+      link: 'https://huggingface.co/collections/OpenLLM-Ro/romistral-6647241bf2cbbe9fd8c2afa4',
+      date: '2024-05-17',
+      type: 'Fine-Tune Release',
+      text: 'New fine-tuned based on Mistral: OpenLLM-Ro/romistral',
+    },
+    {
+      link: 'htps://huggingface.co/collections/OpenLLM-Ro/rollama2-664722bbf536ec14701ec81d',
+      date: '2024-05-14',
+      type: 'Fine-Tune Release',
+      text: 'New fine-tuned based on Llama2: OpenLLM-Ro/rollama2',
+    },
+    {
+      link: '/comunicat.pdf',
+      date: '2024-05-14',
+      type: 'Press',
+      text: 'Press release statement regarding the launch of OpenLLM-Ro.',
+    },
+  ];
 
-    <News link={'https://huggingface.co/OpenLLM-Ro'}>
-      09.10.2024 - released new instruct and human aligned models
-    </News>
+  const typeEmojis: Record<(typeof entries)[number]['type'], string> = {
+    Paper: '📝',
+    Release: '🎉',
+    'Fine-Tune Release': '🤖',
+    Press: '📰',
+    Code: '💻',
+    Event: '🗓',
+  };
 
-    <News link={'https://ilds.ro/workshop-llms-for-romanian'}>
-      28.09.2024 - workshop LLMs for Romanian
-    </News>
+  return (
+    <Stack padding={['8vh 10vw']} gap={[10]}>
+      {Object.entries(
+        entries
+          .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+          .reduce<Record<string, typeof entries>>((acc, entry) => {
+            const date = new Date(entry.date);
+            const year = date.getFullYear();
+            const month = date.toLocaleString('default', { month: 'long' });
+            const key = `${year}-${month}`;
 
-    <News link={'https://github.com/OpenLLM-Ro/FastChat'}>
-      26.08.2024 - released the code & data (RoMTBench and RoCulturaBench) for evaluating Romanian
-      LLMs using LLM-as-a-Judge
-    </News>
-
-    <News link={'https://github.com/OpenLLM-Ro/LLaMA-Factory'}>
-      14.08.2024 - released the code & data for training Romanian LLMs
-    </News>
-
-    <News link={'https://github.com/OpenLLM-Ro/lm-evaluation-harness'}>
-      09.08.2024 - released the code & data for evaluating Romanian LLMs on academic benchmarks and
-      downstream tasks
-    </News>
-
-    <News link={'https://huggingface.co/collections/OpenLLM-Ro/rollama3-667d25e2db6a8e980f0b1551'}>
-      28.06.2024 - launched a new Romanian model fine-tuned on Llama3
-    </News>
-
-    <News link={'https://huggingface.co/collections/OpenLLM-Ro/rogemma-667d261f6b607a23173c1e73'}>
-      28.06.2024 - launched a new Romanian model fine-tuned on Gemma
-    </News>
-
-    <News link={'https://arxiv.org/abs/2406.18266'}>
-      27.06.2024 - new preprint adding more models and evaluations
-    </News>
-
-    <News link={'https://huggingface.co/collections/OpenLLM-Ro/romistral-6647241bf2cbbe9fd8c2afa4'}>
-      17.05.2024 - launched a new Romanian model fine-tuned on Mistral
-    </News>
-
-    <News link={'https://huggingface.co/collections/OpenLLM-Ro/rollama2-664722bbf536ec14701ec81d'}>
-      14.05.2024 - launched the first Romanian models fine-tuned on Llama2
-    </News>
-
-    <News link={'/comunicat.pdf'}>
-      14.05.2024 - press release (in Romanian) regarding the launch of OpenLLM-Ro
-    </News>
-  </Stack>
-);
+            return {
+              ...acc,
+              [key]: [...((acc[key] as typeof entries) || []), entry],
+            };
+          }, {})
+      ).map(([monthWithYear, news]) => {
+        const [year, month] = monthWithYear.split('-');
+        return (
+          <Stack key={monthWithYear} gap={2}>
+            <Text as={'kbd'} fontWeight={'bold'}>
+              {month} {year}
+            </Text>
+            <Stack>
+              {news.map(({ link, text, date, type }) => (
+                <NewsEntry key={link} link={link}>
+                  <Text fontWeight={'bold'} as={'span'}>
+                    {typeEmojis[type]} {type}
+                  </Text>
+                  : {text} ({new Date(date).toLocaleDateString('ro')})
+                </NewsEntry>
+              ))}
+            </Stack>
+          </Stack>
+        );
+      })}
+    </Stack>
+  );
+};
